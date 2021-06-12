f497b48c1e8cdf5edb7faa368c86e5253d901cd0
tdserapio <monsterbob2008@gmail.com>
2021-06-12 21:26:27 +0800

    Some stuff

--- source_code/main.py
+++ source_code/main.py
@@ -1,1 +1,21 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+from kivy.lang import Builder
+from kivy_garden.mapview import MapView
 
+# Just load our .kv design stuff right here
+Builder.load_string(open('main.kv', 'r').read())
+
+
+class HomePage(Widget):
+    pass
+
+
+class THE_APP(App):
+    def build(self):
+        return HomePage()
+
+
+if __name__ == '__main__':
+    THE_APP().run()
